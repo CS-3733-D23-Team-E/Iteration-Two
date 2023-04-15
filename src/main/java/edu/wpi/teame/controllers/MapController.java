1b1e10cf22bd2696a2f0544262c7f781d457bbc6
Kevin Siegall <digimonlord2@gmail.com>
2023-04-14 22:47:20 -0400

    Added DFS and cleaned up Pathfinding

--- src/main/java/edu/wpi/teame/controllers/MapController.java
+++ src/main/java/edu/wpi/teame/controllers/MapController.java
@@ -6,7 +6,7 @@ import static javafx.scene.paint.Color.WHITE;
 import edu.wpi.teame.Database.SQLRepo;
 import edu.wpi.teame.map.Floor;
 import edu.wpi.teame.map.HospitalNode;
-import edu.wpi.teame.map.pathfinding.AStarPathfinder;
+import edu.wpi.teame.map.pathfinding.AbstractPathfinder;
 import edu.wpi.teame.utilities.*;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import java.util.ArrayList;
@@ -174,7 +174,7 @@ public class MapController {
       return;
     }
     refreshPath();
-    AStarPathfinder pf = new AStarPathfinder();
+    AbstractPathfinder pf = AbstractPathfinder.getInstance("A*");
 
     String toNodeID = SQLRepo.INSTANCE.getNodeIDFromName(to) + "";
     String fromNodeID = SQLRepo.INSTANCE.getNodeIDFromName(from) + "";
