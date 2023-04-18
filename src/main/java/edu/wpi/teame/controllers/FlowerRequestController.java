63fa055dd6650ff995adc232e183d13c71c99c7a
Megan Jacques <mjacques@wpi.edu>
2023-04-17 20:41:12 -0400

    All seperate service tabs finished
    Just missing link to employee databsase

--- src/main/java/edu/wpi/teame/controllers/FlowerRequestController.java
+++ src/main/java/edu/wpi/teame/controllers/FlowerRequestController.java
@@ -6,11 +6,12 @@ import edu.wpi.teame.map.LocationName;
 import edu.wpi.teame.utilities.Navigation;
 import edu.wpi.teame.utilities.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
-import io.github.palexdev.materialfx.controls.MFXTextField;
 import java.util.stream.Stream;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import org.controlsfx.control.SearchableComboBox;
 import org.json.JSONObject;
 
@@ -21,23 +22,31 @@ public class FlowerRequestController implements IRequestController {
 
   ObservableList<String> flowerNum =
       FXCollections.observableArrayList(
-          "1", "2", "Small Bouquet (6)", "Medium Bouquet (8)", "Large Bouquet (12)");
+          "1 - $5",
+          "2 - $10",
+          "Small Bouquet (6) - $15",
+          "Medium Bouquet (8) - $18",
+          "Large Bouquet (12) - $20");
+
+  ObservableList<String> yesNo = FXCollections.observableArrayList("yes", "no");
 
   ObservableList<String> deliveryTimes =
       FXCollections.observableArrayList(
           "10am - 11am", "11am - 12pm", "12pm - 1pm", "1pm - 2pm", "2pm - 3pm", "3pm - 4pm");
 
-  @FXML MFXButton returnButton;
   @FXML MFXButton submitButton;
-  @FXML MFXTextField recipientName;
+  @FXML TextField recipientName;
   @FXML SearchableComboBox roomName;
+  @FXML DatePicker deliveryDate;
   @FXML SearchableComboBox<String> deliveryTime;
   @FXML SearchableComboBox<String> flowerChoice;
   @FXML SearchableComboBox<String> numOfFlowers;
-  @FXML MFXTextField notes;
-  @FXML MFXTextField assignedStaff;
+  @FXML SearchableComboBox<String> cardQuestion;
+  @FXML TextField cardMessage;
+  @FXML TextField notes;
+  @FXML SearchableComboBox<String> assignedStaff;
   @FXML MFXButton cancelButton;
-  @FXML MFXButton clearForm;
+  @FXML MFXButton resetButton;
 
   @FXML
   public void initialize() {
@@ -63,10 +72,11 @@ public class FlowerRequestController implements IRequestController {
     flowerChoice.setItems(flowerChoices);
     numOfFlowers.setItems(flowerNum);
     deliveryTime.setItems(deliveryTimes);
+    cardQuestion.setItems(yesNo);
     // Initialize the buttons
     submitButton.setOnMouseClicked(event -> sendRequest());
     cancelButton.setOnMouseClicked(event -> cancelRequest());
-    clearForm.setOnMouseClicked(event -> clearForm());
+    resetButton.setOnMouseClicked(event -> clearForm());
   }
 
   public ServiceRequestData sendRequest() {
@@ -75,7 +85,10 @@ public class FlowerRequestController implements IRequestController {
     JSONObject requestData = new JSONObject();
     requestData.put("flowerChoice", flowerChoice.getValue());
     requestData.put("numOfFlowers", numOfFlowers.getValue());
+    requestData.put("deliveryDate", deliveryDate.getValue());
     requestData.put("deliveryTime", deliveryTime.getValue());
+    requestData.put("cardQuestion", cardQuestion.getValue());
+    requestData.put("cardMessage", cardMessage.getText());
     requestData.put("recipientName", recipientName.getText());
     requestData.put("roomName", roomName.getValue());
     requestData.put("notes", notes.getText());
@@ -86,7 +99,7 @@ public class FlowerRequestController implements IRequestController {
             ServiceRequestData.RequestType.FLOWERDELIVERY,
             requestData,
             ServiceRequestData.Status.PENDING,
-            assignedStaff.getText());
+            assignedStaff.getValue());
 
     // Return to the home screen
     Navigation.navigate(Screen.HOME);
@@ -104,10 +117,13 @@ public class FlowerRequestController implements IRequestController {
   public void clearForm() {
     flowerChoice.setValue(null);
     numOfFlowers.setValue(null);
+    deliveryDate.setValue(null);
     deliveryTime.setValue(null);
     roomName.setValue(null);
+    cardQuestion.setValue(null);
+    cardMessage.clear();
     recipientName.clear();
     notes.clear();
-    assignedStaff.clear();
+    assignedStaff.setValue(null);
   }
 }
--- src/main/java/edu/wpi/teame/controllers/SignageController.java
+++ src/main/java/edu/wpi/teame/controllers/SignageController.java
@@ -7,10 +7,8 @@ import edu.wpi.teame.utilities.Navigation;
 import edu.wpi.teame.utilities.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 public class SignageController {
@@ -18,45 +16,17 @@ public class SignageController {
   @FXML MFXButton loginButton;
   @FXML MFXTextField usernameField;
   @FXML MFXTextField passwordField;
-  @FXML MFXButton menuButton;
-  @FXML MFXButton menuBarHome;
-  @FXML MFXButton menuBarServices;
-  @FXML MFXButton menuBarMaps;
-  @FXML MFXButton menuBarDatabase;
-  @FXML MFXButton menuBarSignage;
-  @FXML MFXButton menuBarBlank;
-  @FXML MFXButton menuBarExit;
-  @FXML VBox menuBar;
   @FXML StackPane loginStack;
 
   // TODO: Make login work
-  boolean menuVisibilty = false;
+
   boolean loginVisible = false;
 
   public void initialize() {
     // Initially set the menu bar to invisible
     SQLRepo.INSTANCE.connectToDatabase("teame", "teame50");
-    menuBarVisible(false);
+
     loginPopout(false);
-    menuButton.setDisable(true);
-
-    // makes the buttons get highlighted when the mouse hovers over them
-    mouseSetup(menuBarHome);
-    mouseSetup(menuBarServices);
-    mouseSetup(menuBarMaps);
-    mouseSetup(menuBarDatabase);
-    mouseSetup(menuBarExit);
-    mouseSetup(menuBarSignage);
-
-    // When the menu button is clicked, invert the value of menuVisibility and set the menu bar to
-    // that value
-    // (so each time the menu button is clicked it changes the visibility of menu bar back and
-    // forth)
-    menuButton.setOnMouseClicked(
-        event -> {
-          menuVisibilty = !menuVisibilty;
-          menuBarVisible(menuVisibilty);
-        });
 
     userButton.setOnMouseClicked(
         event -> {
@@ -68,29 +38,6 @@ public class SignageController {
         event -> {
           Navigation.navigate(Screen.HOME);
         });
-
-    // Navigation controls for the button in the menu bar
-    menuBarHome.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
-    menuBarServices.setOnMouseClicked(event -> Navigation.navigate(Screen.SERVICE_REQUESTS));
-    menuBarSignage.setOnMouseClicked(
-        event -> {
-          Navigation.navigate(Screen.SIGNAGE_TEXT);
-          menuVisibilty = !menuVisibilty;
-        });
-    menuBarMaps.setOnMouseClicked(event -> Navigation.navigate(Screen.MAP));
-    menuBarDatabase.setOnMouseClicked(event -> Navigation.navigate(Screen.DATABASE_EDITOR));
-    menuBarExit.setOnMouseClicked((event -> Platform.exit()));
-  }
-
-  public void menuBarVisible(boolean bool) {
-    menuBarHome.setVisible(bool);
-    menuBarServices.setVisible(bool);
-    menuBarSignage.setVisible(bool);
-    menuBarMaps.setVisible(bool);
-    menuBarDatabase.setVisible(bool);
-    menuBarExit.setVisible(bool);
-    menuBarBlank.setVisible(bool);
-    menuBar.setVisible(bool);
   }
 
   public void loginPopout(boolean bool) {
