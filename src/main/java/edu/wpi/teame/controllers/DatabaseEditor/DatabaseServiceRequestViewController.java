15f0a99f31e27570723e1dd5ca19511472fd8a84
braeden swain <bhswain@wpi.edu>
2023-04-18 19:06:33 -0400

    existing flower requests should appear now
    Basically copied what I did for meals but still not working, I'll figure it out

--- src/main/java/edu/wpi/teame/controllers/DatabaseEditor/DatabaseServiceRequestViewController.java
+++ src/main/java/edu/wpi/teame/controllers/DatabaseEditor/DatabaseServiceRequestViewController.java
@@ -1,15 +1,15 @@
 package edu.wpi.teame.controllers.DatabaseEditor;
 
 import edu.wpi.teame.Database.SQLRepo;
+import edu.wpi.teame.controllers.FlowerRequestController;
+import edu.wpi.teame.entities.FlowerRequestData;
 import edu.wpi.teame.entities.MealRequestData;
 import edu.wpi.teame.map.*;
 import io.github.palexdev.materialfx.controls.MFXButton;
-import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class DatabaseServiceRequestViewController {
@@ -24,19 +24,6 @@ public class DatabaseServiceRequestViewController {
   @FXML Tab conferenceRoomTab;
   @FXML Tab furnitureTab;
 
-  // fields for Meals
-  @FXML HBox mealAddZone;
-  @FXML MFXTextField mealRecipientNameField;
-  @FXML MFXTextField mealRoomField;
-  @FXML MFXTextField mealDateField;
-  @FXML MFXTextField mealTimeField;
-  @FXML MFXTextField mealStaffField;
-  @FXML MFXTextField mealMainCourseField;
-  @FXML MFXTextField mealSideCourseField;
-  @FXML MFXTextField mealDrinkField;
-  @FXML MFXTextField mealAllergiesField;
-  @FXML MFXTextField mealNotesField;
-
   // table data for Meals
   @FXML TableView<MealRequestData> mealTable;
   @FXML TableColumn<MealRequestData, String> mealRecipientNameCol;
@@ -50,19 +37,28 @@ public class DatabaseServiceRequestViewController {
   @FXML TableColumn<MealRequestData, String> mealAllergiesCol;
   @FXML TableColumn<MealRequestData, String> mealNotesCol;
 
+  // table data for Flowers
+  @FXML TableView<FlowerRequestController> flowerTable;
+  @FXML TableColumn<FlowerRequestData, String> flowerRecipientNameCol;
+  @FXML TableColumn<FlowerRequestData, String> flowerRoomCol;
+  @FXML TableColumn<FlowerRequestData, String> flowerDateCol;
+  @FXML TableColumn<FlowerRequestData, String> flowerTimeCol;
+  @FXML TableColumn<FlowerRequestData, String> flowerStaffCol;
+  @FXML TableColumn<FlowerRequestData, String> flowerFlowerChoiceCol;
+  @FXML TableColumn<FlowerRequestData, String> flowerNumberOfFlowersCol;
+  @FXML TableColumn<FlowerRequestData, String> flowerIncludeACardCol;
+  @FXML TableColumn<FlowerRequestData, String> flowerCardMessageCol;
+  @FXML TableColumn<FlowerRequestData, String> flowerNotesCol;
+
   //////////////////////////////////////////
   @FXML VBox editMealZone;
-  @FXML MFXTextField editMealRecipientNameField;
-  @FXML ComboBox<String> editRoomField;
-  @FXML MFXTextField editNodeYField;
-  @FXML ComboBox<Floor> editNodeFloorChoice;
-  @FXML ComboBox<String> editNodeBuildingChoice;
 
   @FXML
   public void initialize() {
 
     SQLRepo dC = SQLRepo.INSTANCE;
 
+    // fill table for meal requests
     mealRecipientNameCol.setCellValueFactory(
         new PropertyValueFactory<MealRequestData, String>("recipientName"));
     mealRoomCol.setCellValueFactory(new PropertyValueFactory<MealRequestData, String>("room"));
@@ -89,5 +85,36 @@ public class DatabaseServiceRequestViewController {
               }
             });
     mealTable.setEditable(true);
+
+    // fill table for flower requests
+    flowerRecipientNameCol.setCellValueFactory(
+        new PropertyValueFactory<FlowerRequestData, String>("recipientName"));
+    flowerRoomCol.setCellValueFactory(new PropertyValueFactory<FlowerRequestData, String>("room"));
+    flowerDateCol.setCellValueFactory(new PropertyValueFactory<FlowerRequestData, String>("date"));
+    flowerTimeCol.setCellValueFactory(new PropertyValueFactory<FlowerRequestData, String>("time"));
+    flowerStaffCol.setCellValueFactory(
+        new PropertyValueFactory<FlowerRequestData, String>("staff"));
+    flowerFlowerChoiceCol.setCellValueFactory(
+        new PropertyValueFactory<FlowerRequestData, String>("flowerChoice"));
+    flowerNumberOfFlowersCol.setCellValueFactory(
+        new PropertyValueFactory<FlowerRequestData, String>("numberOfFlowers"));
+    flowerIncludeACardCol.setCellValueFactory(
+        new PropertyValueFactory<FlowerRequestData, String>("includeACard"));
+    flowerCardMessageCol.setCellValueFactory(
+        new PropertyValueFactory<FlowerRequestData, String>("cardMessage"));
+    flowerNotesCol.setCellValueFactory(
+        new PropertyValueFactory<FlowerRequestData, String>("notes"));
+
+    flowerTable.setItems(FXCollections.observableArrayList());
+    flowerTable
+        .getSelectionModel()
+        .selectedItemProperty()
+        .addListener(
+            (obs, oldSelection, newSelection) -> {
+              if (newSelection != null) {
+                // displayMoveEdit(newSelection);
+              }
+            });
+    flowerTable.setEditable(true);
   }
 }
