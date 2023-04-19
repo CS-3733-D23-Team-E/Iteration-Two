879324ceda52022ab9b87d0afa52fd935f79a988
DiyarJaafar <116843342+DiyarJaafar@users.noreply.github.com>
2023-04-18 20:13:14 -0400

    test

--- src/main/java/edu/wpi/teame/controllers/DatabaseEditor/MoveComponentController.java
+++ src/main/java/edu/wpi/teame/controllers/DatabaseEditor/MoveComponentController.java
@@ -1,10 +1,25 @@
 package edu.wpi.teame.controllers.DatabaseEditor;
 
+import io.github.palexdev.materialfx.controls.MFXComboBox;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Tab;
 
 public class MoveComponentController {
+    @FXML MFXComboBox departmentMoveSelector;
+    @FXML MFXComboBox newNodeSelector;
+    @FXML MFXComboBox departmentOneSelector;
+    @FXML MFXComboBox departmentTwoSelector;
+    @FXML DatePicker moveDateSelector;
+    @FXML Tab moveTab;
+    @FXML Tab swapTab;
+
 
     @FXML public void initialize(){
+        
+    }
+
+    private void swapDepartments(){
         
     }
 }
